65406705403d6ead6ae45834da61309528fd44fa
Nathaniel Bond <nabond251@gmail.com>
2023-11-11 11:24:07 -0500

    Add sources/sinks

---
+++ CoroutineCommandLine/CommandLineSink.cs
@@ -0,0 +1,10 @@
+﻿namespace CoroutineCommandLine;
+
+public record CommandLineSink
+{
+    public record ReadLine() : CommandLineSink();
+
+    public record WriteLine(string Text) : CommandLineSink();
+
+    private CommandLineSink() { }
+}
