e8d946e7733800aaefba57dbf819569f1384e36a
Nathaniel Bond <nabond251@gmail.com>
2023-11-11 11:06:58 -0500

    Add initial interpreter

--- CoroutineCommandLine/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace CoroutineCommandLine
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ CoroutineCommandLine/CommandLineInterpreter.cs
@@ -0,0 +1,19 @@
+﻿namespace CoroutineCommandLine;
+
+public static class CommandLineInterpreter
+{
+    public static void Interpret(IEnumerable<string?> program)
+    {
+        foreach (var i in program)
+        {
+            if (i is null)
+            {
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
