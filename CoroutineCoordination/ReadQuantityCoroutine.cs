43a4e8608c2fcf18159a9c810429975a420b97d3
Nathaniel Bond <nabond251@gmail.com>
2023-11-19 21:39:55 -0500

    Implement read quantity

---
+++ CoroutineCoordination/ReadQuantityCoroutine.cs
@@ -0,0 +1,36 @@
+﻿namespace CoroutineCoordination;
+
+using System.Collections;
+using CoroutineCommandLine;
+
+public class ReadQuantityCoroutine : IEnumerable<ReadQuantitySink>
+{
+    public CommandLineSource? NextValue { protected get; set; }
+
+    public IEnumerator<ReadQuantitySink> GetEnumerator()
+    {
+        yield return new ReadQuantitySink.CommandLine(
+            new CommandLineSink.WriteLine("Please enter number of diners:"));
+
+        yield return new ReadQuantitySink.CommandLine(
+            new CommandLineSink.ReadLine());
+        var l = this.NextValue as CommandLineSource.ReadLine ??
+            throw new InvalidOperationException();
+
+        if (int.TryParse(l.Text, out var dinerCount))
+        {
+            yield return new ReadQuantitySink.Response(dinerCount);
+        }
+        else
+        {
+            yield return new ReadQuantitySink.CommandLine(
+                new CommandLineSink.WriteLine("Not an integer."));
+
+            yield return new ReadQuantitySink.Call(
+                new ReadQuantityCoroutine());
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() =>
+        this.GetEnumerator();
+}
---
+++ CoroutineCoordination/ReadQuantitySink.cs
@@ -0,0 +1,14 @@
+﻿using CoroutineCommandLine;
+
+namespace CoroutineCoordination;
+
+public record ReadQuantitySink
+{
+    public record CommandLine(CommandLineSink Sink) : ReadQuantitySink();
+
+    public record Call(ReadQuantityCoroutine ReadQuantity) : ReadQuantitySink();
+
+    public record Response(int Quantity) : ReadQuantitySink();
+
+    private ReadQuantitySink() { }
+}
