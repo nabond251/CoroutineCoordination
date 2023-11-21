0d32fe25d49b32cb15f21e4cfe29b2eea5621ffe
Nathaniel Bond <nabond251@gmail.com>
2023-11-20 21:51:01 -0500

    Add base class

---
+++ Coroutine/BaseCoroutine.cs
@@ -0,0 +1,21 @@
+﻿namespace Coroutine;
+
+using System.Collections;
+using System.Threading.Channels;
+
+public abstract class BaseCoroutine<T, TReturn, TNext> : IEnumerable<T>
+{
+    protected BaseCoroutine(ChannelReader<TNext> next)
+    {
+        this.Next = next;
+    }
+
+    public ChannelReader<TNext> Next { get; }
+
+    public TReturn? Return { get; protected set; }
+
+    public abstract IEnumerator<T> GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator() =>
+        this.GetEnumerator();
+}
