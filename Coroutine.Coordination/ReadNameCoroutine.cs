515c49451d7e8f3506fd2d99cb15cb4595e68a1a
Nathaniel Bond <nabond251@gmail.com>
2023-11-22 22:51:16 -0500

    Add string readers

---
+++ Coroutine.Coordination/ReadNameCoroutine.cs
@@ -0,0 +1,14 @@
+﻿namespace Coroutine.Coordination;
+
+public class ReadNameCoroutine : CommandLineCoroutine<string>
+{
+    public override IEnumerator<CommandLineEffect> GetEnumerator()
+    {
+        yield return new CommandLineEffect.WriteLine(
+            "Please enter your name:");
+
+        var name = new CommandLineEffect.ReadLine();
+        yield return name;
+        Result = name.Result;
+    }
+}
