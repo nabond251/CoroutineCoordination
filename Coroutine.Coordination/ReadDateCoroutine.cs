b87e0be3d59a03ef9148825946f5f28139793b6e
Nathaniel Bond <nabond251@gmail.com>
2023-11-22 22:49:47 -0500

    Add date reader

---
+++ Coroutine.Coordination/ReadDateCoroutine.cs
@@ -0,0 +1,29 @@
+﻿namespace Coroutine.Coordination;
+
+public class ReadDateCoroutine : CommandLineCoroutine<DateTime>
+{
+    public override IEnumerator<CommandLineEffect> GetEnumerator()
+    {
+        yield return new CommandLineEffect.WriteLine(
+            "Please enter your desired date:");
+
+        var date = new CommandLineEffect.ReadLine();
+        yield return date;
+        var l = date.Result;
+
+        if (DateTime.TryParse(l, out var dt))
+        {
+            Result = dt;
+        }
+        else
+        {
+            yield return new CommandLineEffect.WriteLine(
+                "Not a date.");
+
+            var readDate = new CommandLineEffect.Call<DateTime>(
+                new ReadDateCoroutine());
+            yield return readDate;
+            Result = readDate.Result;
+        }
+    }
+}
